514d339180a52cc2aaccc574cbaa375f3565118a
joychenke <joyyouxiang@163.com>
2021-10-05 10:32:12 +0800

    feat: 用内联法使用expose-loader

--- src/info.js
+++ src/info.js
@@ -1,2 +1,6 @@
+import $day from "expose-loader?exposes=$day!dayjs"
+const d = $day()
+console.log(d)
+
 const dom = $('.info')
 dom.addClass('white')
--- src/name.js
+++ src/name.js
@@ -1,1 +1,3 @@
+const time = $day().format('YYYY-MM-DD')
+console.log(time)
 module.exports = "hahaha11"
